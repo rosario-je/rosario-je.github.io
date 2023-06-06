acd29db65608d4eba85c17f4d820920ce1ba5356
Jose Eduardo <josenxs12@gmail.com>
2023-06-06 15:07:51 -0400

    Js: text changes in accordance to current image

--- main.js
+++ main.js
@@ -29,9 +29,11 @@
 // ---------------------------MODAL WINDOW JS---------------------------
 
 // ---------------------------MODAL WINDOW #1---------------------------
+  
 document.addEventListener("DOMContentLoaded", function () {
-  var modalImages1 = document.querySelectorAll(".modal1"); // Update the class name accordingly for the first modal
-  var mainImage1 = document.querySelector(".modal-img.main-image"); // Update the class name accordingly for the first modal
+  var modalImages1 = document.querySelectorAll(".modal1");
+  var mainImage1 = document.querySelector(".modal-img.main-image");
+  var descriptionContainer = document.querySelector(".modal-body .row:last-child .col");
 
   modalImages1.forEach(function (image) {
     image.addEventListener("click", function () {
@@ -41,16 +43,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
       this.src = mainImageSrc;
       mainImage1.src = clickedImageSrc;
-
       modalImages1[clickedImageIndex].src = clickedImageSrc;
+
+      // Update the description text based on the clicked image
+      var descriptions = [
+        "Main Project description goes here",
+        "Second image description",
+        "Third image description",
+        "Fourth image description",
+        "Fifth image description",
+        "6th image description",
+      ];
+
+      descriptionContainer.innerHTML = descriptions[clickedImageIndex];
     });
   });
 
-  var modal1 = document.getElementById("project1-modal"); // Update the modal ID accordingly for the first modal
+  var modal1 = document.getElementById("project1-modal");
   modal1.addEventListener("hidden.bs.modal", function () {
-    mainImage1.src = "/images/project 1/project 1.jpg"; // Update with the path to your main image for the first modal
+    mainImage1.src = "/images/project 1/project 1.jpg";
+    descriptionContainer.innerHTML = "Main Project description goes here";
   });
-  
+
 
 
 
@@ -59,94 +73,148 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ---------------------------MODAL WINDOW #2---------------------------
 
-  var modalImages2 = document.querySelectorAll(".modal2"); // Update the class name accordingly for the second modal
-  var mainImage2 = document.querySelector(".modal-img2.main-image2"); // Update the class name accordingly for the second modal
-
-  modalImages2.forEach(function (image) {
-    image.addEventListener("click", function () {
-      var clickedImageSrc = this.src;
-      var mainImageSrc = mainImage2.src;
-      var clickedImageIndex = Array.from(modalImages2).indexOf(this);
-
-      this.src = mainImageSrc;
-      mainImage2.src = clickedImageSrc;
-
-      modalImages2[clickedImageIndex].src = clickedImageSrc;
+    var modalImages2 = document.querySelectorAll(".modal2");
+    var mainImage2 = document.querySelector(".modal-img2.main-image2");
+    var descriptionContainer2 = document.querySelector("#project2-modal .modal-body .row:last-child .col");
+  
+    modalImages2.forEach(function (image) {
+      image.addEventListener("click", function () {
+        var clickedImageSrc = this.src;
+        var mainImageSrc = mainImage2.src;
+        var clickedImageIndex = Array.from(modalImages2).indexOf(this);
+  
+        this.src = mainImageSrc;
+        mainImage2.src = clickedImageSrc;
+        modalImages2[clickedImageIndex].src = clickedImageSrc;
+  
+        // Update the description text based on the clicked image
+        var descriptions = [
+        "Second project main description",
+        "Second image description",
+        "Third image description",
+        "Fourth image description",
+        "Fifth image description",
+        "6th image description",
+        ];
+  
+        descriptionContainer2.innerHTML = descriptions[clickedImageIndex];
+      });
     });
-  });
+  
+    var modal2 = document.getElementById("project2-modal");
+    modal2.addEventListener("hidden.bs.modal", function () {
+      mainImage2.src = "/images/project 2/project 2.jpg";
+      descriptionContainer2.innerHTML = "Second project main description";
+    });
 
-  var modal2 = document.getElementById("project2-modal"); // Update the modal ID accordingly for the first modal
-  modal2.addEventListener("hidden.bs.modal", function () {
-    mainImage2.src = "/images/project 2/project 2.jpg"; // Update with the path to your main image for the first modal
-  });
+  
 
   // ---------------------------MODAL WINDOW #3---------------------------
 
-  var modalImages3 = document.querySelectorAll(".modal3"); // Update the class name accordingly for the second modal
-  var mainImage3 = document.querySelector(".modal-img3.main-image3"); // Update the class name accordingly for the second modal
-
-  modalImages3.forEach(function (image) {
-    image.addEventListener("click", function () {
-      var clickedImageSrc = this.src;
-      var mainImageSrc = mainImage3.src;
-      var clickedImageIndex = Array.from(modalImages3).indexOf(this);
-
-      this.src = mainImageSrc;
-      mainImage3.src = clickedImageSrc;
-
-      modalImages3[clickedImageIndex].src = clickedImageSrc;
+  var modalImages3 = document.querySelectorAll(".modal3");
+    var mainImage3 = document.querySelector(".modal-img3.main-image3");
+    var descriptionContainer3 = document.querySelector("#project3-modal .modal-body .row:last-child .col");
+  
+    modalImages3.forEach(function (image) {
+      image.addEventListener("click", function () {
+        var clickedImageSrc = this.src;
+        var mainImageSrc = mainImage3.src;
+        var clickedImageIndex = Array.from(modalImages3).indexOf(this);
+  
+        this.src = mainImageSrc;
+        mainImage3.src = clickedImageSrc;
+        modalImages3[clickedImageIndex].src = clickedImageSrc;
+  
+        // Update the description text based on the clicked image
+        var descriptions = [
+        "Third project main description",
+        "Second image description",
+        "Third image description",
+        "Fourth image description",
+        "Fifth image description",
+        "6th image description",
+        ];
+  
+        descriptionContainer3.innerHTML = descriptions[clickedImageIndex];
+      });
     });
-  });
-
-  var modal3 = document.getElementById("project3-modal"); // Update the modal ID accordingly for the first modal
-  modal3.addEventListener("hidden.bs.modal", function () {
-    mainImage3.src = "/images/project 3/project 3.jpg"; // Update with the path to your main image for the first modal
-  });
+  
+    var modal3 = document.getElementById("project3-modal");
+    modal3.addEventListener("hidden.bs.modal", function () {
+      mainImage3.src = "/images/project 3/project 3.jpg";
+      descriptionContainer3.innerHTML = "Third project main description";
+    });
 
   // ---------------------------MODAL WINDOW #4---------------------------
 
-  var modalImages4 = document.querySelectorAll(".modal4"); // Update the class name accordingly for the second modal
-  var mainImage4 = document.querySelector(".modal-img4.main-image4"); // Update the class name accordingly for the second modal
-
-  modalImages4.forEach(function (image) {
-    image.addEventListener("click", function () {
-      var clickedImageSrc = this.src;
-      var mainImageSrc = mainImage4.src;
-      var clickedImageIndex = Array.from(modalImages4).indexOf(this);
-
-      this.src = mainImageSrc;
-      mainImage4.src = clickedImageSrc;
-
-      modalImages4[clickedImageIndex].src = clickedImageSrc;
+  var modalImages4 = document.querySelectorAll(".modal4");
+    var mainImage4 = document.querySelector(".modal-img4.main-image4");
+    var descriptionContainer4 = document.querySelector("#project4-modal .modal-body .row:last-child .col");
+  
+    modalImages4.forEach(function (image) {
+      image.addEventListener("click", function () {
+        var clickedImageSrc = this.src;
+        var mainImageSrc = mainImage4.src;
+        var clickedImageIndex = Array.from(modalImages4).indexOf(this);
+  
+        this.src = mainImageSrc;
+        mainImage4.src = clickedImageSrc;
+        modalImages4[clickedImageIndex].src = clickedImageSrc;
+  
+        // Update the description text based on the clicked image
+        var descriptions = [
+        "Fourth project main description",
+        "Second image description",
+        "Third image description",
+        "Fourth image description",
+        "Fifth image description",
+        "6th image description",
+        ];
+  
+        descriptionContainer4.innerHTML = descriptions[clickedImageIndex];
+      });
     });
-  });
-
-  var modal4 = document.getElementById("project4-modal"); // Update the modal ID accordingly for the first modal
-  modal4.addEventListener("hidden.bs.modal", function () {
-    mainImage4.src = "/images/project 4/project 4.jpg"; // Update with the path to your main image for the first modal
-  });
+  
+    var modal4 = document.getElementById("project4-modal");
+    modal4.addEventListener("hidden.bs.modal", function () {
+      mainImage4.src = "/images/project 4/project 4.jpg";
+      descriptionContainer4.innerHTML = "Fourth project main description";
+    });
   // ---------------------------MODAL WINDOW #5---------------------------
 
-  var modalImages5 = document.querySelectorAll(".modal5"); // Update the class name accordingly for the second modal
-  var mainImage5 = document.querySelector(".modal-img5.main-image5"); // Update the class name accordingly for the second modal
-
-  modalImages5.forEach(function (image) {
-    image.addEventListener("click", function () {
-      var clickedImageSrc = this.src;
-      var mainImageSrc = mainImage5.src;
-      var clickedImageIndex = Array.from(modalImages5).indexOf(this);
-
-      this.src = mainImageSrc;
-      mainImage5.src = clickedImageSrc;
-
-      modalImages5[clickedImageIndex].src = clickedImageSrc;
+  var modalImages5 = document.querySelectorAll(".modal5");
+    var mainImage5 = document.querySelector(".modal-img5.main-image5");
+    var descriptionContainer5 = document.querySelector("#project5-modal .modal-body .row:last-child .col");
+  
+    modalImages5.forEach(function (image) {
+      image.addEventListener("click", function () {
+        var clickedImageSrc = this.src;
+        var mainImageSrc = mainImage5.src;
+        var clickedImageIndex = Array.from(modalImages5).indexOf(this);
+  
+        this.src = mainImageSrc;
+        mainImage5.src = clickedImageSrc;
+        modalImages5[clickedImageIndex].src = clickedImageSrc;
+  
+        // Update the description text based on the clicked image
+        var descriptions = [
+        "5th project main description",
+        "Second image description",
+        "Third image description",
+        "Fourth image description",
+        "Fifth image description",
+        "6th image description",
+        ];
+  
+        descriptionContainer5.innerHTML = descriptions[clickedImageIndex];
+      });
     });
-  });
-
-  var modal5 = document.getElementById("project5-modal"); // Update the modal ID accordingly for the first modal
-  modal5.addEventListener("hidden.bs.modal", function () {
-    mainImage5.src = "/images/project 5/project 5.jpg"; // Update with the path to your main image for the first modal
-  });
+  
+    var modal5 = document.getElementById("project5-modal");
+    modal5.addEventListener("hidden.bs.modal", function () {
+      mainImage5.src = "/images/project 5/project 5.jpg";
+      descriptionContainer5.innerHTML = "5th project main description";
+    });
 
 
 
